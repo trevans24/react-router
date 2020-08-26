4cd8056b3ecc13c7ded4a161e98d1de2e6c6f647
Troy <trevans24@gmail.com>
2020-08-26 12:45:56 -0600

    working on nested routes

--- src/Pages/BlogPost.js
+++ src/Pages/BlogPost.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useParams, withRouter } from 'react-router-dom';
+
 // Components
 const BlogPost = (props) => {
+  const { blogId } = useParams();
+
+  console.log('Blog Post', blogId)
+
   return (
     <div className="">
       BlogPost works
     </div>
   )
 };
-export default BlogPost;
+export default withRouter(BlogPost);
